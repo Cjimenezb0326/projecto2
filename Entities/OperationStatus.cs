f0cca6d17bef6ab93f53063706d3f35ed773ade4
ItsLean <132397824+ItsLean@users.noreply.github.com>
2024-10-09 18:28:00 -0600

    Revert "Commit OperationStatus"
    This reverts commit 593107ae5ba0139dd766729d0bd23f90f37de79e.

--- Entities/OperationStatus.cs
+++ Entities/OperationStatus.cs
@@ -1,12 +1,14 @@
 ﻿namespace Entities
 {
+
     public enum OperationStatus
     {
         Success,
         Failure,
-        TableNotFound,    // Maneja el caso en que no se encuentra la tabla
-        TableNotEmpty,    // Maneja el caso en que la tabla no está vacía
-        RowNotFound,      // Agregado para manejar el caso en que no se encuentran filas para eliminar
-        DeleteFailed      // Agregado para manejar cualquier fallo durante la operación de eliminación
+        TableNotFound,// Agregado para manejar el estado cuando la tabla no se encuentra
+        // Otros estados según sea necesario
+        TableNotEmpty, 
     }
+
+
 }
